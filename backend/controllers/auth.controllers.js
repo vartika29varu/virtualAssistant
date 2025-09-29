66edd5dd6ff388dc49f7b1861db9a73dfe1de87e
vartika29varu <shuklavartika886@gmail.com>
2025-09-29 19:27:50 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,8 +27,8 @@ export const signUp=async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:false
+            sameSite:"None",
+            secure:true
         })
 
         return res.status(201).json(user)
